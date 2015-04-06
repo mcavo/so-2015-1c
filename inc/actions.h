a6382bbb1949187c051194ed9397b6a9151e1fd4
mcavo <mvictoria.cavo@gmail.com>
2015-04-06 11:34:53 -0300

    lala

--- inc/actions.h
+++ inc/actions.h
@@ -5,10 +5,12 @@ typedef uint8_t ticket_t;
 
 typedef struct movie {
     char* name;
-    uint16_t size;
-    uint16_t remaining_tickets;
+    uint16_t fil;
+    uint16_t col;
     ticket_t* tickets;
 } movie_t;
 
 
 void action_buy_tickets();
+
+void action_show_movies();
