066afadd8563e3d33f597077982002e5354cb935
AngieArlanti <angie.arlanti@gmail.com>
2015-04-06 17:56:09 -0300

    db_connector bloquea, lee y escribe

--- src/db_connector.c
+++ src/db_connector.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 #define CLEAN_BUFF	while ( getchar() != '\n' );
 
@@ -10,30 +11,71 @@
 int main() {
 	struct flock fl = {F_WRLCK, SEEK_SET, 0, 0, 0 };
     int fd;
-    printf("%d\n",sizeof(char));
+	int i=0;	
+	char c;
+
     fl.l_pid = getpid();
 
-    if ((fd = open("../database/malefica.txt", O_WRONLY)) == -1) {
+//LEVANTO ARCHIVO
+
+    if ((fd = open("../database/malefica.txt", O_RDWR)) == -1) {
         perror("open");
         exit(1);
     }
 
-    fl.l_start  = 4;        /* Offset from l_whence         */
-	fl.l_len    = 2;        /* length, 0 = to EOF           */
-    printf("Press <RETURN> to try to get lock: ");
-    getchar();
-    printf("Trying to get lock...");
+//SETEO DONDE BLOQUEAR 
 
-    if (fcntl(fd, F_SETLKW, &fl) == -1) {
+    fl.l_start  = 0;        /* 0 = Offset from l_whence         */
+	fl.l_len    = 0;        /* length, 0 = to EOF           */
+
+//INTENTO BLOQUEAR
+    printf("Press <RETURN> to try to get lock: \n");
+    getchar();
+    printf("Trying to get lock...\n");
+	
+    if(fcntl(fd, F_SETLK, &fl) == -1){
+	printf("Trying to connect to database...\n");
+	if (fcntl(fd, F_SETLKW, &fl) == -1) {
         perror("fcntl");
         exit(1);
-    }
 
+    	}
+
+     }
+
+
+    
     printf("got lock\n");
 
-    write(fd,"la",2);
+//LEER ESE ASIENTO
+	
+        lseek(fd, 9, SEEK_SET);	
+	read(fd,&c,1); 
+	//c = fgetc(fd);
+	if(c=='1'){
+		printf("Los asientos estan ocupados. Intente nuevamente. \n");
 
-    printf("Press <RETURN> to release lock: ");
+		fl.l_type = F_UNLCK;  /* set to unlock same region */
+
+
+	    if (fcntl(fd, F_SETLK, &fl) == -1) {
+		perror("fcntl");
+		exit(1);
+	    }
+
+	    printf("Unlocked.\n");
+ 		exit(1);
+		
+	}
+	
+
+//OCUPAR ASIENTO
+    lseek(fd, 9, SEEK_SET);
+    write(fd,"2",1);
+	
+    
+
+    printf("Press <RETURN> to release lock: \n");
     CLEAN_BUFF
     getchar();
 
