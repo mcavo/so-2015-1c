c095ea46aa37ceec8e65526c7676c89609e7e6fc
mcavo <mcavo@itba.edu.ar>
2015-04-30 15:58:19 -0300

    agrego lo de euge con algunas correciones importantes

--- B/src/exe/client.c
+++ B/src/exe/client.c
@@ -1,52 +1,39 @@
 #include "../../inc/front.h"
+#include "../../inc/utils.h"
 
-#define clean while(getchar()!='\n')
 
 static void showCinemaTitle();
-
 static int showMenu();
-
-static int get_server_pid();
 
 
 int main(int argc, char** argv) {
-
 	int action;
 	int server_pid = get_server_pid();
-
 	if(server_pid==-1){
 		printf("Error server_pid.txt\n");
 		exit(0);
-	}	
-	
+	}
 	showCinemaTitle();
-
 	ipc_t *ipc = ipc_connect(server_pid);
-	ipc_t *ipc_res;
 	int pid=getpid();
-	uint16_t movie_id=0;
-
 	while ( (action = showMenu())!=ACTION_EXIT ) {
-
-		ipc_res = ipc_connect(pid);
-		printf("Cliente abre ok ipc_res id: %d, pid_cliente: %d\n",ipc_res->id ,pid);  
-
+		printf("Cliente abre ok pid_cliente: %d\n", pid);
 		switch (action) {
-			case ACTION_SHOW_FIXTURE:				
-				actionShowFixture(ipc, ipc_res);
+			case ACTION_SHOW_FIXTURE:
+				actionShowFixture(ipc);
 				break;
 			case ACTION_BUY_TICKETS:
-				actionBuyTickets(ipc, ipc_res);
+				actionBuyTickets(ipc);
 				break;
-			case ACTION_PRINT_CINEMA:
-				actionPrintCinema(ipc, ipc_res);
+				case ACTION_PRINT_CINEMA:
+				actionPrintCinema(ipc);
+				break;
 			default:
 				printf("Invalid option.\n");
 				break;
 		}
 	}
-	printf("\n\nGoodbye!\n");
-
+	printf("\n\n  Goodbye!\n\n");
 	return 0;
 }
 
@@ -76,6 +63,7 @@ static void showCinemaTitle () {
 
 }
 
+
 static int showMenu(){
     int choice = -1;
         printf("\n  **********\n\
@@ -88,7 +76,7 @@ static int showMenu(){
   Please, select your choice:\n\
     ");
   scanf("%d", &choice);
-  clean;
+  CLEAN_BUFFER;
 
   return choice;
 }
